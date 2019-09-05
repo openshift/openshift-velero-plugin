33935a81d4ed6fca8299100dedf91730c9d28208
Scott Seago <sseago@redhat.com>
2019-09-05 11:39:19 -0400

    Don't restore replicasets/RCs belonging to deployments/DCs

--- velero-plugins/replicationcontroller/restore.go
+++ velero-plugins/replicationcontroller/restore.go
@@ -38,6 +38,19 @@ func (p *RestorePlugin) Execute(input *velero.RestoreItemActionExecuteInput) (*v
 	common.SwapContainerImageRefs(replicationController.Spec.Template.Spec.Containers, backupRegistry, registry, p.Log)
 	common.SwapContainerImageRefs(replicationController.Spec.Template.Spec.InitContainers, backupRegistry, registry, p.Log)
 
+	ownerRefs, err := common.GetOwnerReferences(input.ItemFromBackup)
+	if err != nil {
+		return nil, err
+	}
+	// Don't restore ReplicationController if owned by DeploymentConfig
+	for i := range ownerRefs {
+		ref := ownerRefs[i]
+		if ref.Kind == "DeploymentConfig" {
+			p.Log.Infof("[replicationcontroller-restore] skipping restore of ReplicationController %s, belongs to DeploymentConfig", replicationController.Name)
+			return velero.NewRestoreItemActionExecuteOutput(input.Item).WithoutRestore(), nil
+		}
+	}
+
 	var out map[string]interface{}
 	objrec, _ := json.Marshal(replicationController)
 	json.Unmarshal(objrec, &out)
