15a71a545cbc5be1949b9910fbd68049ec42e50d
jgabani <jgabani@localhost.localdomain>
2020-06-03 15:01:25 -0400

    Adding support for backup of service accounts

--- velero-plugins/common/types.go
+++ velero-plugins/common/types.go
@@ -14,8 +14,10 @@ type APIServerConfig struct {
 	RoutingConfig     routingConfig     `json:"routingConfig"`
 }
 
+
 const BackupServerVersion string = "openshift.io/backup-server-version"
 const RestoreServerVersion string = "openshift.io/restore-server-version"
 
 const BackupRegistryHostname string = "openshift.io/backup-registry-hostname"
 const RestoreRegistryHostname string = "openshift.io/restore-registry-hostname"
+
