e7018241e0d1dce42aca233b9131f6eca3c139dc
Jaydipkumar Arvindbhai Gabani <gabanijaydip@gmail.com>
2020-06-08 12:01:33 -0400

    Add: PVC and PV plugin code (#29)
    * Add: serviceaccount plugins code Modified: go version in makefile
    
    * Update: order of the regitry in main.go
    
    * Add: Pv and PVC pluggin from migration repo
    
    Co-authored-by: jgabani <jgabani@localhost.localdomain>

--- velero-plugins/common/types.go
+++ velero-plugins/common/types.go
@@ -19,3 +19,19 @@ const RestoreServerVersion string = "openshift.io/restore-server-version"
 
 const BackupRegistryHostname string = "openshift.io/backup-registry-hostname"
 const RestoreRegistryHostname string = "openshift.io/restore-registry-hostname"
+
+// copy, swing, TODO: others (snapshot, custom, etc.)
+const MigrateTypeAnnotation string = "openshift.io/migrate-type"
+
+// target storage class
+const MigrateStorageClassAnnotation string = "openshift.io/target-storage-class"
+
+//target access mode
+const MigrateAccessModeAnnotation string = "openshift.io/target-access-mode"
+
+// kubernetes PVC annotations
+const PVCSelectedNodeAnnotation string = "volume.kubernetes.io/selected-node"
+
+// distinction for B/R and migration
+const MigrationApplicationLabelKey string = "app.kubernetes.io/part-of"
+const MigrationApplicationLabelValue string = "openshift-migration"
