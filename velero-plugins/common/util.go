1bb49ed47a31d03d7946985b0650cde296e1c796
Amoghrd <aramesha@redhat.com>
2020-06-09 09:51:12 -0400

    Update of util.go

--- velero-plugins/common/util.go
+++ velero-plugins/common/util.go
@@ -152,15 +152,3 @@ func GetOwnerReferences(item runtime.Unstructured) ([]metav1.OwnerReference, err
 	}
 	return metadata.GetOwnerReferences(), nil
 }
-
-// ConfigureContainerSleep replaces the pod, cmd, and arg on containers so that
-// post stage or migrate phase 1 restores applications do not start
-func ConfigureContainerSleep(containers []corev1API.Container, duration string) {
-	for n, _ := range containers {
-		if containers[n].Name != "restic-wait" {
-			containers[n].Image = "registry.access.redhat.com/rhel7"
-			containers[n].Command = []string{"sleep"}
-			containers[n].Args = []string{duration}
-		}
-	}
-}
