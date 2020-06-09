c40e53f024b0a450153a892bbda1e4ff1e4eb0dd
devarshshah15 <devarshshah98@gmail.com>
2020-06-09 15:41:26 -0400

    Added trailing spaces as commented in PR

--- velero-plugins/common/types.go
+++ velero-plugins/common/types.go
@@ -1,14 +1,14 @@
 package common
 type routingConfig struct {
-   Subdomain string `json:"subdomain"`
+      Subdomain string `json:"subdomain"`
 }
 type imagePolicyConfig struct {
-   InternalRegistryHostname string `json:"internalRegistryHostname"`
+      InternalRegistryHostname string `json:"internalRegistryHostname"`
 }
 // APIServerConfig stores configuration information about the current cluster
 type APIServerConfig struct {
-   ImagePolicyConfig imagePolicyConfig `json:"imagePolicyConfig"`
-   RoutingConfig     routingConfig     `json:"routingConfig"`
+      ImagePolicyConfig imagePolicyConfig `json:"imagePolicyConfig"`
+      RoutingConfig     routingConfig     `json:"routingConfig"`
 }
 const BackupServerVersion string = "openshift.io/backup-server-version"
 const RestoreServerVersion string = "openshift.io/restore-server-version"
