27a38f59785cedc3b822fe2d9283dc897efdca5c
Jaydipkumar Arvindbhai Gabani <gabanijaydip@gmail.com>
2020-07-09 13:17:22 -0400

    Add: code for B/R Imagestream plugin (#40)
    * Imagestream plugin for backup and restore
    
    * Add: error messages for imagestream
    
    * Adding vendor and mod file
    
    * Constant variable for configmap
    
    * Update configmap name
    
    * Update destination path
    
    * Code refactoring for imagestream
    
    * Added annotations for backup location in common B/R file
    
    * Add: comments on the code
    
    * Update: comments and route function name
    
    * Update: retry loop for imagestream

--- velero-plugins/imagestream/shared.go
+++ velero-plugins/imagestream/shared.go
@@ -1,25 +1,26 @@
 package imagestream
+
 import (
 	"context"
 	"errors"
 	"fmt"
-
 	"github.com/containers/image/v5/copy"
 	"github.com/containers/image/v5/signature"
 	"github.com/containers/image/v5/transports/alltransports"
 	"github.com/containers/image/v5/types"
 	imagev1API "github.com/openshift/api/image/v1"
-
+	"github.com/sirupsen/logrus"
 	"k8s.io/client-go/rest"
+	"strings"
+	"time"
 )
 
-func copyImage(src, dest string, sourceCtx, destinationCtx *types.SystemContext) ([]byte, error) {
+func copyImage(log logrus.FieldLogger,src, dest string, sourceCtx, destinationCtx *types.SystemContext) ([]byte, error) {
 	policyContext, err := getPolicyContext()
 	if err != nil {
 		return []byte{}, fmt.Errorf("Error loading trust policy: %v", err)
 	}
 	defer policyContext.Destroy()
-
 	srcRef, err := alltransports.ParseImageName(src)
 	if err != nil {
 		return []byte{}, fmt.Errorf("Invalid source name %s: %v", src, err)
@@ -28,11 +29,27 @@ func copyImage(src, dest string, sourceCtx, destinationCtx *types.SystemContext)
 	if err != nil {
 		return []byte{}, fmt.Errorf("Invalid destination name %s: %v", dest, err)
 	}
-	manifest, err := copy.Image(context.Background(), policyContext, destRef, srcRef, &copy.Options{
-		SourceCtx:      sourceCtx,
-		DestinationCtx: destinationCtx,
-	})
-	return manifest, err
+	// Let's retry the image copy up to 10 times
+	// Each retry will wait 5 seconds longer
+	// Let's log a warning if we encounter `blob unknown to registry`
+	retryWait := 5
+	log.Info(fmt.Sprintf("copying image: %s; will attempt up to 5 times...", src))
+	for i := 0; i < 5; i++ {
+		manifest, err := copy.Image(context.Background(), policyContext, destRef, srcRef, &copy.Options{
+			SourceCtx:      sourceCtx,
+			DestinationCtx: destinationCtx,
+		})
+		if err == nil {
+			return manifest, err
+		}
+		if strings.Contains(err.Error(), "blob unknown to registry") {
+			log.Warn(fmt.Sprintf("encountered `blob unknown to registry error` for image %s", src))
+		}
+		log.Info(fmt.Sprintf("attempt #%v failed, waiting %vs and then retrying", i, retryWait))
+		time.Sleep(time.Duration(retryWait) * time.Second)
+		retryWait += 5
+	}
+	return []byte{}, err
 }
 
 func getPolicyContext() (*signature.PolicyContext, error) {
@@ -77,6 +94,3 @@ func findSpecTag(tags []imagev1API.TagReference, name string) *imagev1API.TagRef
 	}
 	return nil
 }
-
-
-
