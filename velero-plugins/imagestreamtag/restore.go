b17838c61445415a097025671b05575b59824bfa
Scott Seago <sseago@redhat.com>
2023-03-21 17:56:37 -0400

    OADP-144: fix race condition for istag restore plugin

--- velero-plugins/imagestreamtag/restore.go
+++ velero-plugins/imagestreamtag/restore.go
@@ -1,20 +1,30 @@
 package imagestreamtag
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 
+	"github.com/konveyor/openshift-velero-plugin/velero-plugins/clients"
 	"github.com/konveyor/openshift-velero-plugin/velero-plugins/common"
 	imagev1API "github.com/openshift/api/image/v1"
 	"github.com/sirupsen/logrus"
+	v1 "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
 	"github.com/vmware-tanzu/velero/pkg/plugin/velero"
+	riav2 "github.com/vmware-tanzu/velero/pkg/plugin/velero/restoreitemaction/v2"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 // RestorePlugin is a restore item action plugin for Velero
 type RestorePlugin struct {
 	Log logrus.FieldLogger
+}
+
+// This won't be called but is needed to implement interface
+func (p *RestorePlugin) Name() string {
+	return "istag-restore"
 }
 
 // AppliesTo returns a velero.ResourceSelector that applies to imagestreamtags
@@ -91,10 +101,51 @@ func (p *RestorePlugin) Execute(input *velero.RestoreItemActionExecuteInput) (*v
 
 		p.Log.Info("[istag-restore] Restoring reference or remote imagestreamtag")
 		return &velero.RestoreItemActionExecuteOutput{
-			UpdatedItem:     input.Item,
-			AdditionalItems: additionalItems,
+			UpdatedItem:            input.Item,
+			AdditionalItems:        additionalItems,
+			WaitForAdditionalItems: true,
 		}, nil
 	}
 	p.Log.Info("[istag-restore] Not restoring local imagestreamtag")
 	return velero.NewRestoreItemActionExecuteOutput(input.Item).WithoutRestore(), nil
+}
+
+func (p *RestorePlugin) Progress(operationID string, restore *v1.Restore) (velero.OperationProgress, error) {
+	return velero.OperationProgress{}, riav2.AsyncOperationsNotSupportedError()
+}
+
+func (p *RestorePlugin) Cancel(operationID string, restore *v1.Restore) error {
+	return nil
+}
+
+func (p *RestorePlugin) AreAdditionalItemsReady(additionalItems []velero.ResourceIdentifier, restore *v1.Restore) (bool, error) {
+	p.Log.Info("[istag-restore] AreAdditionalItemsReady called")
+	ready := true
+	client, err := clients.ImageClient()
+	if err != nil {
+		p.Log.Warn("[istag-restore] AreAdditionalItemsReady ImageClient error: %v", err)
+		return ready, err
+	}
+	namespaceMapping := restore.Spec.NamespaceMapping
+	for _, itemIdentifier := range additionalItems {
+		if itemIdentifier.GroupResource.Group != "image.openshift.io" ||
+			itemIdentifier.GroupResource.Resource != "imagestreamtags" {
+			p.Log.Warn(fmt.Sprintf("[istag-restore] AreAdditionalItemsReady: wrong GroupResource: %v, %v", itemIdentifier.GroupResource.Group, itemIdentifier.GroupResource.Resource))
+			continue
+		}
+		namespace := itemIdentifier.Namespace
+		// check item in target namespace
+		if namespace != "" && namespaceMapping != nil && namespaceMapping[namespace] != "" {
+			namespace = namespaceMapping[namespace]
+		}
+		_, err = client.ImageStreamTags(namespace).Get(context.TODO(), itemIdentifier.Name, metav1.GetOptions{})
+		if err != nil {
+			// istag not ready
+			ready = false
+			p.Log.Info(fmt.Sprintf("[istag-restore] AreAdditionalItemsReady: not ready: %v/%v", itemIdentifier.Namespace, itemIdentifier.Name))
+			break
+		}
+		p.Log.Info(fmt.Sprintf("[istag-restore] AreAdditionalItemsReady: ready: %v/%v", itemIdentifier.Namespace, itemIdentifier.Name))
+	}
+	return ready, nil
 }
