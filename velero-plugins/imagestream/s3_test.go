578fee441bf05c4287e1c0bbb441ec8b28b043de
Scott Seago <sseago@redhat.com>
2026-01-09 13:14:59 -0500

    Mock GetBucketRegion for unit tests

--- velero-plugins/imagestream/s3_test.go
+++ velero-plugins/imagestream/s3_test.go
@@ -7,45 +7,67 @@ func TestGetBucketRegion(t *testing.T) {
 		bucket string
 	}
 	tests := []struct {
-		name    string
-		bucket  string
-		want    string
-		wantErr bool
+		name       string
+		bucket     string
+		mockRegion string
+		mockErr    bool
+		wantRegion string
+		wantErr    bool
 	}{
 		{
 		  	name: "openshift-velero-plugin-s3-auto-region-test-1",
 				bucket: "openshift-velero-plugin-s3-auto-region-test-1",
-				want: "us-east-1",
+				mockRegion: "us-east-1",
+				mockErr: nil,
+				wantRegion: "us-east-1",
 				wantErr: false,
 		},
 		{
 		  	name: "openshift-velero-plugin-s3-auto-region-test-2",
 				bucket: "openshift-velero-plugin-s3-auto-region-test-2",
-				want: "us-west-1",
+				mockRegion: "us-west-1",
+				mockErr: nil,
+				wantRegion: "us-west-1",
 				wantErr: false,
 		},
 		{
 		  	name: "openshift-velero-plugin-s3-auto-region-test-3",
 				bucket: "openshift-velero-plugin-s3-auto-region-test-3",
-				want: "eu-central-1",
+				mockRegion: "eu-central-1",
+				mockErr: nil,
+				wantRegion: "eu-central-1",
 				wantErr: false,
 		},
 		{
 		  	name: "openshift-velero-plugin-s3-auto-region-test-4",
 				bucket: "openshift-velero-plugin-s3-auto-region-test-4",
-				want: "sa-east-1",
+				mockRegion: "sa-east-1",
+				mockErr: nil,
+				wantRegion: "sa-east-1",
 				wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			// Save original and restore after test
+			originalFunc := GetBucketRegionFunc
+			defer func() { GetBucketRegionFunc = originalFunc }()
+
+			// Set up mock
+			GetBucketRegionFunc = func(bucket string) (string, error) {
+				if bucket != tt.bucket {
+					t.Errorf("GetBucketRegion called with wrong bucket: got %v, want %v", bucket, tt.bucket)
+				}
+				return tt.mockRegion, tt.mockErr
+			}
+
 			got, err := GetBucketRegion(tt.bucket)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetBucketRegion() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if got != tt.want {
-				t.Errorf("GetBucketRegion() = %v, want %v", got, tt.want)
+			if got != tt.wantRegion {
+				t.Errorf("GetBucketRegion() = %v, want %v", got, tt.wantRegion)
 			}
 		})
 	}
