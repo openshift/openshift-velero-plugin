585e99bd21dc513d62e1f2b34d857acc6fee9243
Sharoon Austin <shanuaustin11@gmail.com>
2026-09-18 15:07:15 +0530

    OADP-8792 New changes after first round of review

--- velero-plugins/proxy/backup.go
+++ velero-plugins/proxy/backup.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 
-	"github.com/konveyor/openshift-velero-plugin/velero-plugins/clients"
 	configv1 "github.com/openshift/api/config/v1"
 	"github.com/sirupsen/logrus"
 	v1 "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
@@ -13,19 +12,22 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 )
 
 const (
-	MustIncludeAnnotation = "backup.velero.io/must-include-additional-items"
+	mustIncludeAnnotation    = v1.MustIncludeAdditionalItemAnnotation
+	openshiftConfigNamespace = "openshift-config"
 )
 
 type BackupPlugin struct {
-	Log logrus.FieldLogger
+	Log    logrus.FieldLogger
+	Client corev1.CoreV1Interface
 }
 
 func (p *BackupPlugin) AppliesTo() (velero.ResourceSelector, error) {
 	return velero.ResourceSelector{
-		IncludedResources: []string{"proxies"},
+		IncludedResources: []string{"proxies.config.openshift.io"},
 	}, nil
 }
 
@@ -41,48 +43,39 @@ func (p *BackupPlugin) Execute(item runtime.Unstructured, backup *v1.Backup) (ru
 	p.Log.Infof("[proxy-backup] proxy: %s", proxy.Name)
 
 	if proxy.Spec.TrustedCA.Name != "" {
-		p.Log.Infof("[proxy-backup] Proxy %s references ConfigMap: %s/%s", proxy.Name, "openshift-config", proxy.Spec.TrustedCA.Name)
-		cmIdentifier, localerr := p.addConfigMap(proxy.Spec.TrustedCA.Name)
-		if localerr == nil {
+		p.Log.Infof("[proxy-backup] Proxy %s references ConfigMap: %s/%s", proxy.Name, openshiftConfigNamespace, proxy.Spec.TrustedCA.Name)
+		cmIdentifier, localErr := p.addConfigMap(proxy.Spec.TrustedCA.Name)
+		if localErr == nil {
 			additionalItems = append(additionalItems, cmIdentifier)
 		} else {
-			if errors.IsNotFound(localerr) {
-				p.Log.Warnf("[proxy-backup] ConfigMap openshift-config/%s for proxy %s not found: %s", proxy.Spec.TrustedCA.Name, proxy.Name, localerr.Error())
+			if errors.IsNotFound(localErr) {
+				p.Log.Warnf("[proxy-backup] ConfigMap %s/%s for proxy %s not found: %s", openshiftConfigNamespace, proxy.Spec.TrustedCA.Name, proxy.Name, localErr.Error())
 			} else {
-				p.Log.Errorf("[proxy-backup] Error adding ConfigMap openshift-config/%s for proxy %s: %s", proxy.Spec.TrustedCA.Name, proxy.Name, localerr.Error())
-				err = localerr
+				p.Log.Errorf("[proxy-backup] Error adding ConfigMap %s/%s for proxy %s: %s", openshiftConfigNamespace, proxy.Spec.TrustedCA.Name, proxy.Name, localErr.Error())
+				err = localErr
 			}
 		}
 	}
 
-	var out map[string]interface{}
-	objrec, _ := json.Marshal(proxy)
-	json.Unmarshal(objrec, &out)
-
 	if len(additionalItems) > 0 {
+		out := item.UnstructuredContent()
 		if metadata, ok := out["metadata"].(map[string]interface{}); ok {
 			if annotations, ok := metadata["annotations"].(map[string]interface{}); ok {
-				annotations[MustIncludeAnnotation] = "true"
+				annotations[mustIncludeAnnotation] = "true"
 			} else {
 				metadata["annotations"] = map[string]interface{}{
-					MustIncludeAnnotation: "true",
+					mustIncludeAnnotation: "true",
 				}
 			}
 		}
-		p.Log.Infof("[proxy-backup] Set %s annotation on Proxy to include additional items", MustIncludeAnnotation)
+		p.Log.Infof("[proxy-backup] Set %s annotation on Proxy to include additional items", mustIncludeAnnotation)
 	}
 
-	item.SetUnstructuredContent(out)
 	return item, additionalItems, err
 }
 
 func (p *BackupPlugin) addConfigMap(cmName string) (velero.ResourceIdentifier, error) {
-	coreClient, err := clients.CoreClient()
-	if err != nil {
-		return velero.ResourceIdentifier{}, err
-	}
-
-	cm, err := coreClient.ConfigMaps("openshift-config").Get(context.Background(), cmName, metav1.GetOptions{})
+	cm, err := p.Client.ConfigMaps(openshiftConfigNamespace).Get(context.TODO(), cmName, metav1.GetOptions{})
 	if err != nil {
 		return velero.ResourceIdentifier{}, err
 	}
--- velero-plugins/proxy/backup_test.go
+++ velero-plugins/proxy/backup_test.go
@@ -6,10 +6,12 @@ import (
 	configv1 "github.com/openshift/api/config/v1"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
-	v1 "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
+	velerov1 "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/fake"
 )
 
 func TestAppliesTo(t *testing.T) {
@@ -19,7 +21,7 @@ func TestAppliesTo(t *testing.T) {
 
 	selector, err := plugin.AppliesTo()
 	assert.NoError(t, err)
-	assert.Contains(t, selector.IncludedResources, "proxies")
+	assert.Contains(t, selector.IncludedResources, "proxies.config.openshift.io")
 }
 
 func TestExecuteWithoutTrustedCA(t *testing.T) {
@@ -39,15 +41,29 @@ func TestExecuteWithoutTrustedCA(t *testing.T) {
 
 	obj := &unstructured.Unstructured{Object: proxyUnstructured}
 
-	_, additionalItems, err := plugin.Execute(obj, &v1.Backup{})
+	_, additionalItems, err := plugin.Execute(obj, &velerov1.Backup{})
 
 	assert.NoError(t, err)
 	assert.Len(t, additionalItems, 0)
 }
 
 func TestExecuteWithTrustedCA(t *testing.T) {
+	// Create a fake Kubernetes client with the ConfigMap
+	fakeClientset := fake.NewSimpleClientset(
+		&corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "etcd-ca-bundle",
+				Namespace: "openshift-config",
+			},
+			Data: map[string]string{
+				"ca-bundle.crt": "-----BEGIN CERTIFICATE-----\n...\n-----END CERTIFICATE-----",
+			},
+		},
+	)
+
 	plugin := &BackupPlugin{
-		Log: logrus.New(),
+		Log:    logrus.New(),
+		Client: fakeClientset.CoreV1(),
 	}
 
 	proxy := &configv1.Proxy{
@@ -56,7 +72,7 @@ func TestExecuteWithTrustedCA(t *testing.T) {
 		},
 		Spec: configv1.ProxySpec{
 			TrustedCA: configv1.ConfigMapNameReference{
-				Name: "test-ca-bundle",
+				Name: "etcd-ca-bundle",
 			},
 		},
 	}
@@ -66,7 +82,21 @@ func TestExecuteWithTrustedCA(t *testing.T) {
 
 	obj := &unstructured.Unstructured{Object: proxyUnstructured}
 
-	_, _, err = plugin.Execute(obj, &v1.Backup{})
+	modifiedProxy, additionalItems, err := plugin.Execute(obj, &velerov1.Backup{})
 
-	assert.Error(t, err)
+	assert.NoError(t, err)
+	assert.Len(t, additionalItems, 1)
+	assert.Equal(t, "configmaps", additionalItems[0].GroupResource.Resource)
+	assert.Equal(t, "openshift-config", additionalItems[0].Namespace)
+	assert.Equal(t, "etcd-ca-bundle", additionalItems[0].Name)
+
+	metadata, ok := modifiedProxy.UnstructuredContent()["metadata"].(map[string]interface{})
+	assert.True(t, ok, "metadata not found in modified object")
+
+	annotations, ok := metadata["annotations"].(map[string]interface{})
+	assert.True(t, ok, "annotations not found in metadata")
+
+	annotationValue, ok := annotations[velerov1.MustIncludeAdditionalItemAnnotation]
+	assert.True(t, ok, "must-include-additional-items annotation not found")
+	assert.Equal(t, "true", annotationValue)
 }
